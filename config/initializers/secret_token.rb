3e21bc537f616ca68dc61982cec675d516769d69
rails <satish.aher@hotmail.com@.com>
2014-06-18 23:37:10 +0530

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+June::Application.config.secret_key_base = 'd826ea68b416c376769dbde903f670202e1176dbafe64b3b9a11044db2636bb41db204009103b1b4367b527ed00d27f91558bb6c695266503abe1f61534e1042'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+June::Application.config.session_store :cookie_store, key: '_june_session'
